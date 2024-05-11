0b7e5c9c1c83e98b85124dea448e886c4a8ed611
86133 <981695328@qq.com>
2024-05-11 13:57:49 +0800

    将模式转化功能重新添加进来

--- src/App.java
+++ src/App.java
@@ -7,6 +7,8 @@ import com.ui.LoginJFrame;
 public class App {
     public static void main(String[] args) {
         // 创建对象  并显示登录窗口
-        new LoginJFrame();
+        //new LoginJFrame();
+        new GameJFrame();
+        //new GameJFrame2();
     }
 }
